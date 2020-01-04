75828216280be529db071026a4733f6b6ce328b4
krishna <sohala.krishna49@gmail.com>
2020-01-04 12:48:50 +0530

    my first project

---
+++ krish1.com/src/main/java/krishnasohala1/krish1/com/App.java
@@ -0,0 +1,13 @@
+package krishnasohala1.krish1.com;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hiiii krishna!" );
+    }
+}
